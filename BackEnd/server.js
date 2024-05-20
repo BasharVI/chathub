68ee53cc558c77451a8cee211f33adfe2b84df80
Bashar V I <basharvismail@gmail.com>
2024-05-20 11:54:52 +0400

    cofigured authroute and added authmiddleware

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,12 +3,17 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cors = require("cors");
 
+// Routes
+const authRoutes = require("./routes/authRoute");
+
 dotenv.config();
 
 connectDB();
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
